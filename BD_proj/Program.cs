9fab30cbf5d9fc724b358f64216853718a7df07b
Dima6364 <dmitroishak@gmail.com>
2023-12-05 16:16:23 +0200

    Add project files.

---
+++ BD_proj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BD_proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
